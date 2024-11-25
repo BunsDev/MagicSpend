4ce54f16c53c8031d2168a1b7c3e83648a323019
Conner Swenberg <conner.swenberg@coinbase.com>
2024-11-25 15:38:52 -0800

    Update solidity version to support ^0.8.23

--- src/MagicSpend.sol
+++ src/MagicSpend.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {Ownable} from "solady/auth/Ownable.sol";
 import {SignatureCheckerLib} from "solady/utils/SignatureCheckerLib.sol";
